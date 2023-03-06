d836dc93552160f070f3e6cfe315533ecf56a2aa
YannickHecker <yannick.hecker@gmx.de>
2023-03-06 20:29:57 +0100

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sh email_calendar.sh -v --url https://calendar.google.com/calendar/ical/en.german%23holiday%40group.v.calendar.google.com/public/basic.ics
+bash email_calendar.sh -v --url https://calendar.google.com/calendar/ical/en.german%23holiday%40group.v.calendar.google.com/public/basic.ics
